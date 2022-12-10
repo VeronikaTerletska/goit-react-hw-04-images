20d763da2234152187312d584f31be6164a744cd
VeronikaTerletska <104487544+VeronikaTerletska@users.noreply.github.com>
2022-12-10 20:36:21 +0200

    template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { GlobalStyle } from './GlobalStyle';
 export const App = () => {
   return (
     <div
@@ -7,10 +8,11 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
+      <GlobalStyle />
     </div>
   );
 };
